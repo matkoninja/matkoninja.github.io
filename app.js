5ce17d1ffab7297ef7cea874c302e619ff8be60a
Martin Muller <m.muller@upol.cz>
2024-09-16 19:54:17 +0200

    js

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ let prevDom = document.getElementById('prev');
 let carouselDom = document.querySelector('.carousel');
 let listItemDom = document.querySelector('.carousel .list');
 let thumbnailDom = document.querySelector('.carousel .thumbnail');
-let activeImage = listItemDom[itemSlider.length - 1];
 
 nextDom.onclick = function () {
     showSlider('next');
-}
+};
 prevDom.onclick = function () {
     showSlider('prev');
-}
+};
 
 let timeRunning = 3000;
 let timerAutoNext = 10000;
@@ -20,12 +19,13 @@ let runAutoRun = setTimeout(() => {
 }, timerAutoNext);
 
 function showSlider(type) {
+    // itemSlider should be defined here, inside the function where it's being used
     let itemSlider = document.querySelectorAll('.carousel .list .item');
     let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
-
+    
     if (type == 'next') {
         listItemDom.appendChild(itemSlider[0]);
-        thumbnailDom.appendChild(itemThumbnail[0])
+        thumbnailDom.appendChild(itemThumbnail[0]);
         carouselDom.classList.add('next');
     } else {
         let positionLastItem = itemSlider.length - 1;
@@ -34,15 +34,14 @@ function showSlider(type) {
         carouselDom.classList.add('prev');
     }
 
-
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
-    }, timeRunning)
+    }, timeRunning);
 
     clearTimeout(runAutoRun);
     runAutoRun = setTimeout(() => {
         nextDom.click();
-    }, timerAutoNext)
+    }, timerAutoNext);
 }
